b917ba03c17f446386d8303e2d66d97a1ebe6710
aartisingh2020aru-ui <aartisingh2020aru@gmail.com>
2026-03-14 11:57:35 +0530

    php project

---
+++ pdf-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html code in php file!</title>
+</head>
+<body>
+    <h1>This is html tag in php file(.php extension).</h1>
+    <p>but in html(.html) file cannot support php code in it.</p>
+
+    <?php
+        echo "this is php tag in html code. or php extension."
+    ?>
+</body>
+</html>
